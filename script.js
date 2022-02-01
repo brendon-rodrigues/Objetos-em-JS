ca2480aebbafa11f9d3eb4ee2ce2707bd66f995e
Brendon <rodriguesbrendon861@gmail.com>
2022-01-31 21:19:29 -0300

    subindo repositório

---
+++ script.js
@@ -0,0 +1,20 @@
+let brendon = {
+    idade: 20,
+    altura: 1.70,
+    cor: "preto"
+}
+brendon.cabelo = "crespo"
+
+delete brendon.idade
+
+console.log(brendon)
+
+let cadastro = ["nome","idade","telefone", "amigos", "gostos"]
+cadastro ={
+    nome: "brendon",
+    idade: "23 anos",
+    telefone: 40028922,
+    amigos: "Gal, Vitória, Carlos Cadu, Gabs, Jean",
+    gostos: "Machado de Assis, Graciliano Ramos, Galeano, Bk'"
+}
+console.log(cadastro)
